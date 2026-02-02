7c93bdaf938d0b93216ec1f1c9956c04f25c3741
kevin.bedoya32@qmail.cuny.edu <kevin.bedoya32@qmail.cuny.edu>
2026-02-01 23:55:26 -0500

    added new material

---
+++ lab-assignments/lab1_student_gradebook/lab1/Main.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+/*	CSCI212 (111A) Lab #1 : Student Gradebook (
+ *  Full name: 
+ * 	EMPLID: 
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ lab-assignments/lab1_student_gradebook/lab1/StudentGradeBook.java
@@ -0,0 +1,107 @@
+package Lab1; 
+
+/*	CSCI212 (111A) Lab #1 : Student Gradebook 
+ *  Full name: 
+ * 	EMPLID: 
+ */
+
+public class StudentGradeBook {
+	
+
+	
+	// Step 5: Declare private attributes. 
+	
+	// Step 5.
+
+	
+	// Step 6 : Update the constructor. 
+	public StudentGradeBook() {
+		// Assign the name attributes using the 'this' keyword 
+		// Allocate an integer array of size 5 for grades 
+	}
+	// Step 6. 
+	
+	
+	
+	// Step 14: Constructor overloading 
+	/* Write a second constructor that accepts firstName, lastName, and an integer-
+	 * - array of grades. This is an example of 'constructor overloading'. 
+	 */
+	
+	
+	
+	// Step 14.
+	
+	
+	
+	//Step 7: Implement the getter and setter for the student's full name.
+	public String getStudentFullName() { return ""; }
+	
+	public void setStudentFullName() {}
+	//Step 7.  
+	
+	
+	
+	// Step 8: Implement reformatName() 
+	private String reformatName() { 
+	
+		//Takes a String
+		//Returns the same string with the first letter capitalized 
+		
+		return ""; 
+	}
+	//Step 8. 
+	
+	
+	
+	
+	//Step 10: Implement addGrade
+	public void addGrade() {
+		
+		//Valid grades lie in [0,100]
+		//Valid indices lie in [0,4] 
+	}
+	//Step 10. 
+	
+	
+	
+	
+	//Step 11: Implement generateGradeList() 
+	public String generateGradeList() { 
+		
+		//The returned String should consist of the integer grades -
+		//-delimited by commas, with no trailing comma. 
+		
+		return ""; 
+	}
+	//Step 11. 
+	
+	
+	
+
+	//Step 12: Implement the statistical methods
+	
+	public float getMean() { return 0f; }
+	
+	public int getMedian() { return 0; }
+	
+	public int getMaxGrade() { return 0; }
+	
+	public int getMinGrade() { return 0; }
+	
+	//Step 12. 
+	
+	
+	
+	
+	//Step 13: Implement getLetterGrade() 
+	public String getLetterGrade() { return ""; }
+	//Step 13. 
+	
+	
+	//Step 15: Implement generateSummary() 
+	public void generateSummary() {}
+	//Step 15. 
+
+
+}
